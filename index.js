a012e12677852b745d42839bacaebac18fe7d2e1
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-03-19 13:22:56 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+
+function returnFirstTwoDrivers(drivers) {
+  return drivers.slice(0, 2);
+}
+
+function returnLastTwoDrivers(drivers) {
+  return drivers.slice(2, 4);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const multiplier = fare * multiplier;
+function createFareMultiplier(multiplier) {
+  return function (fare) {
+    return fare * multiplier;
+  };
+}
+
+function fareDoubler(fare) {
+  return fare * 2;
+}
+
+function fareTripler(fare) {
+  return fare * 3;
+}
+
+function selectDifferentDrivers(arrayOfDrivers, selectionFunction) {
+  return selectionFunction(arrayOfDrivers);
+}
